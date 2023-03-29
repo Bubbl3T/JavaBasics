fa0a7083cd2f25ea2698e037ce08699c92793d74
Bubbl3T <linzechen@gmail.com>
2023-03-30 00:10:14 +0800

    Exercise 2
    (cherry picked from commit fdf82ebe3ca8200102fadac4b9f55ca5c2e14ee1)

---
+++ 01.BasicJava/Ex2.java
@@ -0,0 +1,47 @@
+public class Ex2 {
+
+    public static void main(String[] args) {
+        {
+            // Q1: Fill in the code so that the for loops stops after printing 3.
+
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+                /* Fill in your code here */
+            }
+        }
+        {
+            // Q2: Print the integer coordinates from (0, 0) to (x, y) (inclusive).
+            // An example has been given below to print (a, b):
+            // int a = 2;
+            // int b = 7;
+            // System.out.printf("(%d, %d)\n", a, b);
+
+            int x = 3;
+            int y = 4;
+            /* Fill in your code here */
+        }
+        {
+            // Q3a: Given n, create an array with values 0 to n-1
+
+            int n = 10;
+            /* Fill in your code here */
+
+            // Q3b: Create another array with values 1 to n
+
+            /* Fill in your code here */
+
+        }
+        {
+            // (Homework) Q4: Construct a matrix with i rows and j columns, with each entry
+            // being the sum of its column index and row index.
+            // i.e. A 2 by 3 matrix should look like this:
+            // [[0, 1, 2],
+            //  [1, 2, 3]]
+
+            int i = 6;
+            int j = 8;
+            /* Fill in your code here */
+        }
+    }
+    
+}
