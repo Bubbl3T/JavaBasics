181f2dfb00b1944e35c2ae06e2f435cdf9586a77
Bubbl3T <linzechen@gmail.com>
2023-03-30 00:35:19 +0800

    Lesson 3 shapes

---
+++ 03.Shapes/Circle.java
@@ -0,0 +1,19 @@
+public class Circle {
+
+    // Parameters
+    /* Fill in your code here */ // The radius of the circle
+    private static final double PI = 3.14;
+
+    // Constructors
+    /* Fill in your code here */
+
+    // Methods
+    /* Fill in your code here */ // Get the area of the circle (Remember to return a double)
+
+    // Overriding (Covered in future classes)
+    @Override
+    public String toString() {
+        return String.format("This is a circle with radius %d", this.radius);
+    }
+    
+}
---
+++ 03.Shapes/Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle {
+
+    // Parameters
+    /* Fill in your code here */ // The length and width of the rectangle
+
+    // Constructors
+    /* Fill in your code here */
+
+    // Methods
+    /* Fill in your code here */ // Get the area of the rectangle
+
+    // Overriding (Covered in future classes)
+    @Override
+    public String toString() {
+        return String.format("This is a rectangle with length %d and width %d", 
+                this.length, this.width);
+    }
+    
+}
